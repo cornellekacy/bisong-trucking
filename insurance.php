4fa82a037bd096d004a773e6228178ba204d3786
cornellekacy <cornellekacy4@gmail.com>
2021-06-14 12:59:16 +0100

    new

--- insurance.php
+++ insurance.php
@@ -24,11 +24,34 @@
 
         <div class="container">
         	<div class="row">
-        		<div class="col-md-4"></div>
-        		<div class="col-md-4">
-        			 
+        		<div class="col-md-2"></div>
+        		<div class="col-md-8">
+        			 <h5>Receiver Interest Cargo Insurance Policy</h5> 
+                     <p>Parcelforce insurance is Receiver interest policy that is also known as “all-risk” coverage or refundeble insurance.
+
+This type of cargo coverage is fairly comprehensive, with fewer exclusions.
+
+The policy covers the actual cargo, not the the carrier’s liability, which means during the shipment process the damage or loss of the goods is covered, although there are aspects of the coverage that may be denied or excluded.  A list of a few of the exclusions include:
+
+Damage Due to Improper Packaging or Loading
+Employee Negligence
+Infestation 
+Abandonmed</p>
+
+<h5>Stander Shipping ( not discreet ) insurance policy Payment</h5>
+<p>All Insurance is calculated depending on the weight and value of the package.
+All package that is shiped with standerd insurance is been calculated as 1% worth the package and is not refundable 
+Custom stamp is $25 not refunded </p>
+ 
+
+
+<h5>Discreet shipping ( Vip Shipping)</h5>
+<p>All Insurance is calculated depending on the weight and value of the package.
+All Discreet Packaging insurance has to be pay 10% worth the package and it will be 98% refunded upon arriver
+Custom Stamp  = $350 refunded complete upon arriver $300</p>
+
         		</div>
-        		<div class="col-md-4"></div>
+        		<div class="col-md-2"></div>
         	</div>
         </div>
 
