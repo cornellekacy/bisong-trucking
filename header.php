d89de958d8b704833fb8a419a58d9d97569d2d36
cornellekacy <cornellekacy4@gmail.com>
2021-06-15 17:46:03 +0100

    new

--- header.php
+++ header.php
@@ -95,8 +95,8 @@
                                     <!-- .navbar-header start -->
                                     <div class="navbar-header"background-color: #000>
                                         <div class="logo">
-                                            <a href="index.html">
-                                                <img src="logof.png" height="50" width="120" alt="Trucking Transportation and Logistics HTML Template"/>
+                                            <a href="index.php">
+                                                <img src="logof.png" height="50" width="120" alt="Parcel Force"/>
                                             </a>
                                         </div><!-- .logo end -->
                                     </div><!-- .navbar-header start -->
@@ -129,99 +129,24 @@
                                             <button class="dl-trigger">Open Menu</button>
 
                                             <ul class="dl-menu">
-                                                <li>
-                                                    <a href="index.php">Home</a>
-                                                    <ul class="dl-submenu">
-                                                        <li><a href="index.html">Home default</a></li>
-                                                        <li><a href="index02.html">Trucking services overview</a></li>
-                                                        <li><a href="index03.html">Trucking video slider</a></li>
-                                                        <li><a href="index04.html">Trucking home simple</a></li>
-                                                        <li><a href="index05.html">Trucking services slider</a></li>
-                                                        <li><a href="index06.html">Trucking parallax slider</a></li>
-                                                        <li><a href="index07.html">Trucking Vehicle Fleet</a></li>
-                                                        <li><a href="index08.html">Trucking corporate</a></li>
-                                                        <li><a href="index09.html">Trucking simple slider</a></li>
-                                                        <li><a href="index10.html">Fullscreen minimal</a></li>
-                                                    </ul><!-- dl-submenu end -->
-                                                </li>
+                                            <li><a href="index.php">Home</a></li>
+                                          <li><a href="about.php">About Us</a></li>
 
-                                                <li>
-                                                    <a href="#">About</a>
-                                                    <ul class="dl-submenu">
-                                                        <li>
-                                                            <a href="#">Header types</a>
-                                                            <ul class="dl-submenu">
-                                                                <li><a href="index.html">Header default</a></li>
-                                                                <li><a href="header-default-top-bar.html">Header default top bar</a></li>
-                                                                <li><a href="index08.html">Header simple light bar</a></li>
-                                                                <li><a href="header-simple-dark-bar.html">Header simple dark bar</a></li>
-                                                                <li><a href="header-simple-dark.html">Header simple dark</a></li>
-                                                                <li><a href="header-menu-bottom.html">Header menu bottom light</a></li>
-                                                                <li><a href="header-menu-bottom-dark.html">Header menu bottom dark</a></li>
-                                                            </ul><!-- .dl-submenu end -->
-                                                        </li>
-                                                        <li><a href="about01.html">About simple</a></li>
-                                                        <li><a href="about02.html">About compact</a></li>
-                                                        <li><a href="about03.html">About with sidebar</a></li>
-                                                        <li><a href="simple-page-title.html">Simple page title</a></li>
-                                                        <li><a href="company-history.html">Company history</a></li>
-                                                        <li><a href="events.html">Company Events</a></li>
-                                                        <li><a href="clients.html">Company Clients</a></li>
-                                                        <li><a href="careers.html">Careers</a></li>
-                                                        <li><a href="driver-application.html">Online driver application</a></li>
-                                                    </ul><!-- dl-submenu end -->
-                                                </li>
+                                        <li><a href="services.php">Servicess</a></li>
 
-                                                <li>
-                                                    <a href="#">Services</a>
-                                                    <ul class="dl-submenu">
-                                                        <li><a href="services01.html">Services overview</a></li>
-                                                        <li>
-                                                            <a href="#">Logistics</a>
-                                                            <ul class="dl-submenu">
-                                                                <li><a href="services02.html">Logistics</a></li>
-                                                                <li><a href="overland-transportation.html">Overland transportation</a></li>
-                                                                <li><a href="air-freight.html">Air freight</a></li>
-                                                                <li><a href="ocean-freight.html">Ocean freight</a></li>
-                                                                <li><a href="large-projects.html">Large projects</a></li>
-                                                                <li><a href="rail-transportation.html">Rail international shipping</a></li>
-                                                                <li><a href="contract-logistics.html">Contract logistics</a></li>
-                                                            </ul><!-- .dl-submenu end -->
-                                                        </li>
-                                                        <li><a href="warehousing.html">Warehousing</a></li>
-                                                        <li><a href="supply-chain-management.html">Supply chain management</a></li>
-                                                        <li><a href="packaging-options.html">Packaging options</a></li>
-                                                        <li><a href="consulting-services.html">Consulting services</a></li>
-                                                    </ul><!-- dl-submenu end -->
-                                                </li>
-
-                                                <li>
-                                                    <a href="#">News</a>
-                                                    <ul class="dl-submenu">
-                                                        <li><a href="news-list.html">News list</a></li>
-                                                        <li><a href="news-masonry.html">News masonry</a></li>
-                                                        <li><a href="news-standard.html">News standard</a></li>
-                                                        <li><a href="news-single.html">News single</a></li>
-                                                    </ul><!-- .dl-submenu end -->
-                                                </li>
-
-                                                <li>
-                                                    <a href="#">Gallery</a>
-                                                    <ul class="dl-submenu">
-                                                        <li><a href="gallery01.html">Vehicle gallery full</a></li>
-                                                        <li><a href="gallery02.html">Vehicle gallery grid</a></li>
-                                                    </ul><!-- .dl-submenu end -->
-                                                </li>
-
-                                                <li><a href="locations.html">Locations</a></li>
+                                        
+                                             <li class="dropdown">
+                                                <a href="#" data-toggle="dropdown" class="dropdown-toggle">Insurance</a>
+                                                <ul class="dropdown-menu">
+                                                    <li><a href="insurance.php">Insurance Policy</a></li>
+                                                    <li><a href="insure-package.php">Insure Package</a></li>
+                                                </ul>
                                                 
-                                                <li>
-                                                    <a href="#">Contact</a>
-                                                    <ul class="dl-submenu">
-                                                        <li><a href="contact-simple.html">Contact simple</a></li>
-                                                        <li><a href="contact-locations.html">Contact locations</a></li>
-                                                    </ul><!-- .dl-submenu end -->
-                                                </li>
+                                              <li><a href="track-package.php">Track Package</a></li>  
+                                             <li><a href="testimonials.php">Testimonials</a></li>
+                                            <li><a href="backend/index.php">Login</a></li>
+
+                                         <li><a href="contact.php">Contact Us</a></li>
                                             </ul><!-- .dl-menu end -->
                                         </div><!-- #dl-menu end -->
 
